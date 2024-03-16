020597e9ccf61a4134e9da69d5ff3314bdb1863a
hammoh7 <soahammohaadkar@gmail.com>
2024-03-16 12:42:56 +0530

    Starter

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex-row items-center justify-center p-20">
+      <p className="flex items-center justify-center">Welcome to Glossify!</p>
+      <p className="flex items-center justify-center">A perfect place to learn a new language</p>
+    </div>
+  );
+}
