b96b622011772aedbb5574982267eb7af0cf7bc2
hammoh7 <soahammohaadkar@gmail.com>
2024-03-23 21:11:07 +0530

    Auth Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return ( 
+        <div className="h-full flex items-center justify-center bg-gradient-to-r from-violet-300 to-indigo-500">
+            {children}
+        </div>
+     );
+}
+ 
+export default AuthLayout;
