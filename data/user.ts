81d79343146d286794e3895befd3f38102053a77
hammoh7 <soahammohaadkar@gmail.com>
2024-03-26 20:46:01 +0530

    Data

---
+++ data/user.ts
@@ -0,0 +1,21 @@
+import { database } from "@/lib/database"
+
+export const getUserByEmail = async (email: string) => {
+    try {
+        const user = await database.user.findUnique({ where: { email } });
+        return user;
+    }
+    catch {
+        return null;
+    }
+}
+
+export const getUserById = async (id: string) => {
+    try {
+        const user = await database.user.findUnique({ where: { id } });
+        return user;
+    }
+    catch {
+        return null;
+    }
+}
