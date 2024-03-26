35a4783be2ccc4e912dec16da230f4924fc98e96
hammoh7 <soahammohaadkar@gmail.com>
2024-03-26 20:45:29 +0530

    Actions

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { database } from "@/lib/database";
 import { RegisterSchema } from "@/schemas";
+import bcrypt  from "bcryptjs";
 import * as z from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,5 +14,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: "Something went wrong!",
     };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email already exists!" }
+  }
+  await database.user.create({
+    data: {
+      name, 
+      email,
+      password: hashedPassword,
+    }
+  })
+
   return { success: "Created!" };
 };
