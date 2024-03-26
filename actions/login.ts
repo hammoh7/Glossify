94a5f52d553ef37bd268d3d0c7ea048a43b15976
hammoh7 <soahammohaadkar@gmail.com>
2024-03-27 03:27:44 +0530

    Actions

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { signIn } from "@/lib/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/lib/route";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -11,5 +14,27 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       error: "Invalid fields!",
     };
   }
-  return { success: "Verified!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Invalid credentials!",
+          };
+        default:
+          return {
+            error: "Something went wrong!",
+          };
+      }
+    }
+    throw error;
+  }
 };
