70fba3be1fff7e037355c42ef31167716b27ba8a
hammoh7 <soahammohaadkar@gmail.com>
2024-03-28 01:56:36 +0530

    App

--- app/(protected)/main/page.tsx
+++ app/(protected)/main/page.tsx
@@ -1,25 +1,10 @@
 "use client";
-
-import { logout } from "@/actions/logout";
-import { Button } from "@/components/ui/button";
-import { currentUser } from "@/hooks/current-user";
+import { LogoutBtn } from "../_components/logout";
 
 const MainPage = () => {
-  const user = currentUser();
-  const onClick = () => {
-    logout();
-  };
   return (
-    <div className=" p-2 rounded-md ml-auto">
-      <Button
-        className="text-white text-md bg-gradient-to-l from-red-400 to-orange-500 rounded-md shadow-md transform transition-transform hover:translate-y-[-3px]"
-        onClick={onClick}
-        type="submit"
-      >
-        Logout
-      </Button>
-    </div>
-  );
+    <LogoutBtn /> 
+  )
 };
 
 export default MainPage;
