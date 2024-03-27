ec3563e6054fe27f0b0fd8fbb7d0c0c552f1781d
hammoh7 <soahammohaadkar@gmail.com>
2024-03-27 17:35:35 +0530

    Library

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -3,9 +3,19 @@ import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import bcrypt from "bcryptjs";
+import google from "next-auth/providers/google";
+import github from "next-auth/providers/github";
 
 export default {
   providers: [
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
@@ -17,7 +27,7 @@ export default {
           if (!user || !user.password) return null;
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
-          
+
           if (passwordsMatch) return user;
         }
         return null;
