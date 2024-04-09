3272b4ad62a56aac3474daaf2e1e5393bef1bc28
hammoh7 <soahammohaadkar@gmail.com>
2024-04-09 16:58:48 +0530

    Main Layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import HeaderMobile from "@/components/start/header-mobile";
+import Sidebar from "@/components/start/sidebar";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <>
+      <HeaderMobile />
+      <Sidebar className="hidden lg:flex" />
+      <main className="h-full lg:pl-[260px] pt-[45px] lg:pt-0">
+        <div className="h-full max-w-[1100px] mx-auto pt-5 bg-gradient-to-r from-violet-300 to-indigo-500">
+          {children}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default MainLayout;
