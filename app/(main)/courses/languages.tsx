b60e9eef5249ac20c12c40c3291be1a570dc7160
hammoh7 <soahammohaadkar@gmail.com>
2024-06-05 00:59:37 +0530

    Courses

--- app/(main)/courses/languages.tsx
+++ app/(main)/courses/languages.tsx
@@ -4,6 +4,8 @@ import { Courses } from "@prisma/client";
 import CourseCard from "./courseCard";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
+import { newUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props = {
   courses: Courses[];
@@ -13,14 +15,16 @@ type Props = {
 const Languages = ({ courses, activeCourseId }: Props) => {
   const router = useRouter();
   const [pending, startTransition] = useTransition();
-  const onClick = (id: string) => {
+  const onClick = (id: number) => {
     if (pending) return;
-    if (id === activeCourseId) {
+    if (id === Number(activeCourseId)) {
       return router.push("/start");
     }
     startTransition(() => {
-      
-    })
+      newUserProgress(String(id)).catch(() =>
+        toast.error("Something went wrong!")
+      );
+    });
   };
 
   return (
@@ -31,8 +35,8 @@ const Languages = ({ courses, activeCourseId }: Props) => {
           id={course.id}
           title={course.title}
           imageSrc={course.imageSrc}
-          onClick={() => {}}
-          disabled={false}
+          onClick={onClick}
+          disabled={pending}
           active={course.id === activeCourseId}
         />
       ))}
