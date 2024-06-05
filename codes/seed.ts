9a7670dd805d52130f2c19b3dcc49c98fe185e3e
hammoh7 <soahammohaadkar@gmail.com>
2024-06-05 11:27:15 +0530

    Seeding

--- codes/seed.ts
+++ codes/seed.ts
@@ -1,43 +1,42 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-    try {
-        console.log("Connecting to MongoDB...");
+  try {
+    console.log("Connecting to MongoDB...");
 
-        // Clear the existing data
-        await prisma.courses.deleteMany();
-        await prisma.userProgress.deleteMany();
+    // Clear the existing data
+    await prisma.courses.deleteMany();
+    await prisma.userProgress.deleteMany();
 
-        await prisma.courses.createMany({
-            data: [
-                {
-                    id: String(1),
-                    title: "Japanese",
-                    imageSrc: "/images/jap.png"
-                },
-                {
-                    id: String(2),
-                    title: "Germany",
-                    imageSrc: "/images/ger.png"
-                },
-                {
-                    id: String(3),
-                    title: "France",
-                    imageSrc: "/images/fra.png"
-                }
-            ]
-        });
-        
-        console.log("Seeding finished");
-    } catch (error) {
-        console.error(error);
-        throw new Error("Failed to seed the database");
-    } finally {
-        await prisma.$disconnect(); 
-    }
+    await prisma.courses.createMany({
+      data: [
+        {
+          id: String(1),
+          title: "Japanese",
+          imageSrc: "/images/jap.png",
+        },
+        {
+          id: String(2),
+          title: "German",
+          imageSrc: "/images/ger.png",
+        },
+        {
+          id: String(3),
+          title: "French",
+          imageSrc: "/images/fra.png",
+        },
+      ],
+    });
+
+    console.log("Seeding finished");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed the database");
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 main();
-
