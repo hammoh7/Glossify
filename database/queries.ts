f7ba556065953dbaed1505923be236bac0f3f22d
hammoh7 <soahammohaadkar@gmail.com>
2024-06-07 18:32:34 +0530

    Database

--- database/queries.ts
+++ database/queries.ts
@@ -31,3 +31,48 @@ export const getCourseById = cache(async (courseId: string) => {
   });
   return data;
 });
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+  const { userId } = await auth();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+  const data = await database.units.findMany({
+    where: {
+      courseId: userProgress.activeCourseId, 
+    },
+    include: {
+      lessons: {
+        include: {
+          challenges: {
+            include: {
+              challengeProgress: {
+                where: {
+                  userId: userId,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonWithCompletedStatus };
+  });
+
+  return normalizedData;
+});
