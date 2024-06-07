ac6c50a18ea13108aeb64222a668a5cede48ccdf
hammoh7 <soahammohaadkar@gmail.com>
2024-06-07 18:32:13 +0530

    App

--- app/(main)/start/page.tsx
+++ app/(main)/start/page.tsx
@@ -2,15 +2,18 @@ import SidebarWrapper from "@/components/start/sidebar-wrapper";
 import UpdateWrapper from "@/components/start/update-wrapper";
 import Header from "./header";
 import UserProgress from "@/components/start/user-progress";
-import { getUserProgress } from "@/database/queries";
+import { getUnits, getUserProgress } from "@/database/queries";
 import { redirect } from "next/navigation";
 
 const StartPage = async () => {
   const userProgressData = getUserProgress();
-  const [userProgress] = await Promise.all([userProgressData]);
+  const unitsData = getUnits();
+  const [userProgress, units] = await Promise.all([userProgressData, unitsData]);
+  
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
+
   return (
     <div className="flex flex-row-reverse gap-[50px] px-5">
       <SidebarWrapper>
@@ -23,6 +26,11 @@ const StartPage = async () => {
       </SidebarWrapper>
       <UpdateWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </UpdateWrapper>
     </div>
   );
