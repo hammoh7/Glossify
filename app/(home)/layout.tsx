eda30c35f480bb9b07b92586966ad0d0572e083a
hammoh7 <soahammohaadkar@gmail.com>
2024-06-08 17:03:20 +0530

    Home layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,24 +1,18 @@
 import { Footer } from "./_components/(footer)/page";
 import Navbar from "./_components/(navbar)/page";
 
-const HomeLayout = ({
-    children
-}: {
-    children: React.ReactNode;
-}) => {
-    return ( 
-        <div className="h-full bg-gradient-to-r from-violet-300 to-indigo-500">
-            <nav>
-                <Navbar />
-            </nav>
-            <main className="pt-28 pb-25">
-                {children}
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </div>
-     );
-}
- 
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-gradient-to-r from-violet-300 to-indigo-500">
+      <nav>
+        <Navbar />
+      </nav>
+      <main className="pt-28 pb-25">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
 export default HomeLayout;
